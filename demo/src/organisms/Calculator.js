3db5771f076add44d242184aefc30b7cd4a19d40
Patrick Smith <patrick@burntcaramel.com>
2017-06-08 01:33:42 +1000

    Clarify props argument

--- demo/src/organisms/Calculator.js
+++ demo/src/organisms/Calculator.js
@@ -3,7 +3,7 @@ import Calculator from '../components/Calculator'
 
 export default makeOrganism(Calculator, {
   initial: ({ initialValue = 0 }) => ({ value: initialValue }),
-  changeValue: ({}, { target }) => ({ value }) => ({ value: parseInt(target.value, 10) }),
+  changeValue: (props, { target }) => ({ value }) => ({ value: parseInt(target.value, 10) }),
   double: () => ({ value }) => ({ value: value * 2 }),
   add3: () => ({ value }) => ({ value: value + 3 })
 })
