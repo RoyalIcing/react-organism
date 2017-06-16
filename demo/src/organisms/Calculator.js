4c651e21933d9389b18ce85ef9d444ed83c54c00
Patrick Smith <patrick@burntcaramel.com>
2017-06-16 23:29:50 +1000

    Fix demo calculator

--- demo/src/organisms/Calculator.js
+++ demo/src/organisms/Calculator.js
@@ -3,7 +3,11 @@ import Calculator from '../components/Calculator'
 
 export default makeOrganism(Calculator, {
   initial: ({ initialValue = 0 }) => ({ value: initialValue }),
-  changeValue: (props, { target }) => ({ value }) => ({ value: parseInt(target.value, 10) }),
+  changeValue: (props, { target }) => ({ value: parseInt(target.value, 10) || '' }),
+  // Or more robust number input handling with fallback to previous value:
+  // changeValue: (props, { target: { value: newValue } }) => ({ value: previousValue }) => ({
+  //   value: newValue && (parseInt(newValue, 10) || previousValue)
+  // }),
   double: () => ({ value }) => ({ value: value * 2 }),
   add3: () => ({ value }) => ({ value: value + 3 })
 })
