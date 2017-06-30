e210c0c51c165e1809d35a1ecca1fab0ad53de2f
Patrick Smith <patrick@burntcaramel.com>
2017-06-30 17:20:05 +1000

    Example: use props passed to state change handler

--- demo/src/organisms/Counter2.js
+++ demo/src/organisms/Counter2.js
@@ -3,6 +3,6 @@ import Counter from '../components/Counter'
 
 export default makeOrganism(Counter, {
   initial: ({ initialCount = 0 }) => ({ count: initialCount }),
-  increment: ({ stride = 1 }) => ({ count }) => ({ count: count + stride }),
-  decrement: ({ stride = 1 }) => ({ count }) => ({ count: count - stride })
+  increment: () => ({ count }, { stride = 1 }) => ({ count: count + stride }),
+  decrement: () => ({ count }, { stride = 1 }) => ({ count: count - stride })
 })
