518d59d02a7dfd98292784331c9875c42a772ba8
Patrick Smith <patrick@burntcaramel.com>
2017-07-05 11:24:01 +1000

    Add simple test

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,10 +1,29 @@
 import expect from 'expect'
 import React from 'react'
 import {render, unmountComponentAtNode} from 'react-dom'
+import ReactTestUtils from 'react-dom/test-utils'
 
-import Component from 'src/'
+import makeOrganism from 'src/'
 
-describe('Component', () => {
+function Counter({
+  count,
+  handlers: {
+    increment,
+    decrement,
+    initial
+  }
+}) {
+  return (
+    <div>
+      <button id='decrement' onClick={ decrement } children='−' />
+      <span>{ count }</span>
+      <button id='increment' onClick={ increment } children='+' />
+      <button id='initial' onClick={ initial } children='Reset' />
+    </div>
+  )
+}
+
+describe('makeOrganism', () => {
   let node
 
   beforeEach(() => {
@@ -15,9 +34,26 @@ describe('Component', () => {
     unmountComponentAtNode(node)
   })
 
-  it('displays a welcome message', () => {
-    render(<Component/>, node, () => {
-      expect(node.innerHTML).toContain('Welcome to React components')
+  it('Sends click events', (done) => {
+    const CounterOrganism = makeOrganism(Counter, {
+      initial: ({ initialCount = 0 }) => ({ count: initialCount }),
+      increment: () => ({ count }) => ({ count: count + 1 }),
+      decrement: () => ({ count }) => ({ count: count - 1 })
+    })
+    const $ = (selector) => node.querySelector(selector)
+    render(<CounterOrganism initialCount={ 2 } />, node, async () => {
+      expect(node.innerHTML).toContain('2')
+
+      // Click increment
+      ReactTestUtils.Simulate.click($('#increment'))
+      expect(node.innerHTML).toContain('3')
+
+      // Click decrement
+      ReactTestUtils.Simulate.click($('#decrement'))
+      expect(node.innerHTML).toContain('2')
+
+      done()
     })
   })
+
 })
