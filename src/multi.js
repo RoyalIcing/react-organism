6b8fcbd520d3a7c8efc178f693648bd339e8d063
Patrick Smith <patrick@burntcaramel.com>
2017-07-18 00:23:19 +1000

    Multi: Ok, now actually fix merging state changes

--- src/multi.js
+++ src/multi.js
@@ -29,7 +29,7 @@ export default function makeMultiOrganism(
           }
           // Else just an object with changes
           else {
-            Object.assign({}, changes[cellKey], stateChanger)
+            changes[cellKey] = Object.assign({}, changes[cellKey], stateChanger)
           }
           return changes
         }
