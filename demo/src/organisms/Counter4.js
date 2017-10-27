68acddbfc2d72cba7837c8a7889f86668b74dbeb
Patrick Smith <patrick@burntcaramel.com>
2017-10-27 18:41:55 +1100

    Clean up example

--- demo/src/organisms/Counter4.js
+++ demo/src/organisms/Counter4.js
@@ -3,13 +3,13 @@ import Counter from '../components/Counter'
 
 export default makeOrganism(Counter, {
   initial: ({ initialCount = 0 }) => ({ count: initialCount }),
-  increment: function * ({ stride = 20, handlers }) {
+  increment: function * ({ stride = 20 }) {
     while (stride > 0) {
       yield ({ count }) => ({ count: count + 1 })
       stride -= 1
     }
   },
-  decrement: function * ({ stride = 20, handlers }) {
+  decrement: function * ({ stride = 20 }) {
     while (stride > 0) {
       yield ({ count }) => ({ count: count - 1 })
       stride -= 1
