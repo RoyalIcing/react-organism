cea9402ea93d3e4b2b7e9d5c980597e121c292e6
Patrick Smith <patrick@burntcaramel.com>
2017-10-27 16:27:40 +1100

    Update animated counter example to use generator function to animate

--- demo/src/organisms/Counter4.js
+++ demo/src/organisms/Counter4.js
@@ -1,21 +1,17 @@
 import makeOrganism from '../../../src'
-import nextFrame from '../../../src/nextFrame'
 import Counter from '../components/Counter'
 
 export default makeOrganism(Counter, {
   initial: ({ initialCount = 0 }) => ({ count: initialCount }),
-  offsetBy: (props, change) => ({ count }) => ({ count: count + change }),
-  increment: async ({ stride = 20, handlers }) => {
+  increment: function * ({ stride = 20, handlers }) {
     while (stride > 0) {
-      await nextFrame()
-      await handlers.offsetBy(1)
+      yield ({ count }) => ({ count: count + 1 })
       stride -= 1
     }
   },
-  decrement: async ({ stride = 20, handlers }) => {
+  decrement: function * ({ stride = 20, handlers }) {
     while (stride > 0) {
-      await nextFrame()
-      await handlers.offsetBy(-1)
+      yield ({ count }) => ({ count: count - 1 })
       stride -= 1
     }
   }
