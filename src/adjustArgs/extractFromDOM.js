8918b858a8d8ab583756bdae3f6ccf2d010a7c8c
Patrick Smith <patrick@burntcaramel.com>
2017-11-14 20:25:05 +1100

    Use awareness’s event values extraction

--- src/adjustArgs/extractFromDOM.js
+++ src/adjustArgs/extractFromDOM.js
@@ -1,58 +1,8 @@
-const numberRegex = /_number$/
+import extractValuesFromDOMEvent from 'awareness/es/extractValuesFromDOMEvent'
 
 export default function extractFromDOM(args) {
-  const targetProperty = 'currentTarget'
-  // If being passed an event with DOM element that has a dataset
-  if (args[0] && args[0][targetProperty] && args[0][targetProperty].dataset) {
-    const event = args[0]
-    const element = event[targetProperty]
-    let values = {
-      value: element.value,
-      checked: element.checked,
-      name: element.name
-    }
-
-    const { dataset } = element
-    const dataKeys = Object.keys(dataset)
-
-    // Read values from data- attributes
-    dataKeys.forEach(dataKey => {
-      let value
-      if (numberRegex.test(dataKey)) {
-        // Read and convert value
-        value = parseFloat(dataset[dataKey])
-        // Strip off _number suffix from final key
-        dataKey = dataKey.replace(numberRegex, '')
-      }
-      else {
-        // Use string value
-        value = dataset[dataKey]
-      }
-      
-      values[dataKey] = value
-    })
-
-    // If submitting a form and data-extract is present on the <form>
-    if (event.type === 'submit' && dataset.extract) {
-      event.preventDefault()
-
-      const { elements } = element
-      // Loop through form elements https://stackoverflow.com/a/19978872
-      for (let i = 0, element; element = elements[i++];) {
-          // Read value from <input>
-          let value = element.value
-          // Handle <input type='number'>
-          if (element.type != null && element.type.toLowerCase() === 'number') {
-            value = parseFloat(value)
-          }
-          values[element.name] = value
-      }
-
-      const reset = !!dataset.reset // data-reset
-      if (reset && element.reset) {
-        element.reset()
-      }
-    }
+  if (args[0]) {
+    const values = extractValuesFromDOMEvent(args[0])
 
     // Place extracted dataset values first, followed by original arguments
     args = [values].concat(args)
